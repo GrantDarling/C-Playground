422d75c26fc9e78bc1e5d063095940cf9e2eb731
Grant Darling <grantcarterdarling@gmail.com>
2022-09-04 00:49:48 -0400

    added comments

--- pun.c
+++ pun.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
 
+
+/* Name: pun.c */ 
+/* Purpose: Prints a bad pun. */ 
+/* Author: Grant C. Darling */
+
 int main(void) {
 	printf("To C or not to C, that is the question \n");
 	return 0;
